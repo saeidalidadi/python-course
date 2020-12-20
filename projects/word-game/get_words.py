8026d6ee249a721ca58d01e6cbfd29d4894d8161
toor <toor@kali.kali>
2020-12-20 13:55:01 -0500

    commit 2

--- projects/word-game/get_words.py
+++ projects/word-game/get_words.py
@@ -1,27 +1,26 @@
 import requests
 import random
 def Words(num=100):
- 	words_list=[]
+	words_list=[]
 
 	get_words=requests.request("GET", "https://api.datamuse.com/words?ml=ringing+in+the+ears&max="+str(num))
 	for i in range(num):
-		print('\n')
 		w = get_words.json()[i]["word"]
 		words_list.append(str(w))
 	return words_list
 
 
 
-def RandWords(num1,num2,num3):
-	words=Words(num1)
+def RandWords(Range_words,Word_length,Number_of_words):
+	words=Words(Range_words)
 	words2=[]
 	for i in words:
-		if len(i) != num2:
+		if len(i) != Word_length:
 			words2.append(i)
 	for j in words2:
 		words.remove(j)
 	del words2
-	rand_words=random.sample(words,k=num3)
+	rand_words=random.sample(words,k=Number_of_words)
 	return rand_words
 
 print(RandWords(100,4,5))

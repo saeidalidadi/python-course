d71ee0deab0a831ea84abba33b33fdbb48d8e110
Ali Masoumiyan <50038415+A-Masoumiyan@users.noreply.github.com>
2020-12-22 13:32:34 -0500

    Update words file.py

--- projects/word-game/words file.py
+++ projects/word-game/words file.py
@@ -1,18 +1,10 @@
 import random
 
-def RandWords(Word_length,Number_of_words):
+def RandWords(Number_of_words):
 	File = open("words.txt", "r")
 	words = File.read().split()
 	File.close()
-
-	words2=[]
-	for i in words:
-		if len(i) != Word_length:
-			words2.append(i)
-	for j in words2:
-		words.remove(j)
-	del words2
 	rand_words=random.sample(words,k=Number_of_words)
 	return rand_words
 
-print(RandWords(4,3))
+print(RandWords(3))

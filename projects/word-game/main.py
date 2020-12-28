3d04b3a5ad23e31a933470aa3c98dbaf7e8384ea
parkhcom <git config user.name khoshnavaz.appdomain@gmail.com>
2020-12-28 18:26:48 +0330

    read-write from file

--- projects/word-game/main.py
+++ projects/word-game/main.py
@@ -1,9 +1,20 @@
 import tkinter as tk
 import table_view as tw
+import random_word as wo
+import random
 
 window = tk.Tk()
 window.title("word game")
 window.geometry("500x500")
-tw.drawTable(window, 5)
+dencity = 5
+
+allLetter = wo.word_object()
+letterArray = wo.set_word_in_array(allLetter, dencity)
+
+tw.drawTable(window, letterArray, dencity)
+ 
+ 
 
 window.mainloop()
+
+ 
--- projects/word-game/table_view.py
+++ projects/word-game/table_view.py
@@ -1,32 +1,35 @@
 import tkinter as tk
 
 import tkinter as tk
-import random 
+import random
 
-alphebet = [
-    ["a","b","c","d","e","y"],
-    ["f","g","h","i","n","k"],
-    ["x","z","h","v","n","k"],
-    ["t","g","h","m","n","k"],
-    ["x","b","h","v","k","k"]
-    ]
-colors = ["#FFFEEE","#FFF" ]
+my_list = []
+def word_object(window, letter, side, color):
+  alphebet1 = list(letter)
+  my_list.append(alphebet1)
+  return my_list
 
-def drawTable(window, side):
+def drawTable(window, alphebet1, side):  
+
   """Create an side*side grids
   Args:
     window (TK): an instance of tkinter window
-    side   (int): the number of rows and columns for grid
+    side (int): the number of rows and columns for grid
   Returns:
     None 
-  """
+  """ 
+  colors = ['#fff','#A3E4D7','#E67E22','#85C1E9','#E74C3C','#8E44AD','#21618C','#5D6D7E','#BDC3C7','#922B21','#F7DC6F','#27AE60','#2E86C1','#2E86C1']
+  i = 0
   for row in range(side):
-    for col in range(side): 
-    #   print(alphebet[row][col]);
-      letter_container = tk.Frame(master=window, relief=tk.RIDGE,  borderwidth=1, background=random.choice(colors),command=ree("Hi"))
-      letter_container.grid(row=row, column=col)
-      label = tk.Label(master=letter_container, text=random.choice(alphebet[row][col]))
-      label.pack(padx=10, pady=10) 
+    color = random.choice(colors)
+    for col in range(side):   
+      if(alphebet1[i] and i<len(alphebet1)-1 ) :
+        letter_container = tk.Frame(master=window, relief=tk.RIDGE,  borderwidth=1, background=color,command=ree("Hi"))
+        letter_container.grid(row=row, column=col)
+        label = tk.Label(master=letter_container, text= alphebet1[row][col]) 
+        label.pack(padx=10, pady=10) 
+       
+      
 
 def ree(hi):
-  a = 4
+  a = 3

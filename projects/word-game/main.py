27800fc9147f8d6a0bdfc09806ee4a3b08e65fcb
arta <arbagherazar@gmail.com>
2021-01-04 14:50:15 +0330

    changes

--- projects/word-game/main.py
+++ projects/word-game/main.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import table_view as tw
+import words as wd
 
 window = tk.Tk()
 window.title("word game")
 window.geometry("500x500")
-tw.drawTable(window, 6)
+
+word_list = wd.readRandomWords(10)
+letters_sequence = wd.wordToLetters(word_list)
+tw.drawTable(window, letters_sequence)
 
 window.mainloop()
--- projects/word-game/table_view.py
+++ projects/word-game/table_view.py
@@ -1,31 +1,16 @@
 import tkinter as tk
-import random
-alphebet =[
-  ["a","b","c","d","e","f"],
-  ["g","h","i","j","k","l"],
-  ["o","m","n","p","q","r"],
-  ["s","t","u","w","x","y"],
-  ["z","f","b","n","h","r"],
-  ["w","f","k","l","q","z"],
-  ]
 
-
-def drawTable(window, side):
-  """Create an side*side grids
-  Args: commit
+def drawTable(window, letters_sequence):
+  """Create an side*side grids with letters.
+  Args:
     window (TK): an instance of tkinter window
-    side   (int): the number of rows and columns for grid
   Returns:
     None 
   """
-  reader = open('word.txt') 
- 
-  lines = reader.read().splitlines()
-  for row in range(side):
-    myline =random.choice(lines)
-    print(myline)
-    for col in range(side):
-      letter_container = tk.Frame(master=window, relief=tk.RIDGE,  borderwidth=6)
+  side_len = len(letters_sequence)
+  for row in range(side_len):
+    for col in range(side_len):
+      letter_container = tk.Frame(master=window, relief=tk.RIDGE, borderwidth=1)
       letter_container.grid(row=row, column=col)
-      label = tk.Label(master=letter_container, text=alphebet[row][col])
-      label.pack(padx=14, pady=13)
+      label = tk.Label(master=letter_container, text=letters_sequence[row][col])
+      label.pack(padx=10, pady=10)
